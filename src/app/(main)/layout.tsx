715d7e86cecd6d74eab0dab52c50564db121ed20
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-08-21 20:16:36 -0300

    ejemplo navbar rapido

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,13 +4,13 @@ import React, { FC, PropsWithChildren } from 'react'
 const UsersLayout: FC<PropsWithChildren> = ({ children }) => {
     return (
       <>
-        <header className='flex justify-between mb-4 px-8 py-4 bg-black'>
+        <header className='flex justify-between mb-4 px-8 py-4 bg-gray-100'>
           <div>LOGO</div>
-          <div>
-            <div>
+          <div className='flex'>
+            <div >
               <Link href="/users">Usuarios</Link>
             </div>
-            <div>
+            <div className='ml-4'>
               <Link href="/messages">Messages</Link>
             </div>
           </div>
