2da9f1e6370a5b531f2e3e14abe80979582cc7ee
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-08-21 21:54:13 -0300

    component funcional parte 2 con ts

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -1,3 +1,4 @@
+import Message from '@/components/messages/Message';
 import Link from 'next/link'
 import React from 'react'
 
@@ -5,16 +6,19 @@ const UserPage = ({params}: {params: {username: string}}) => {
 
   const user = {
     username: params.username,
-    name: 'John Doe',
-    bio: 'John Doe is who I am',
+    name: 'Anakin Skywalker',
+    bio: 'Im from Tatooine, I like to podrace and I am a Jedi Knight. I am the father of Luke Skywalker and Leia Organa.',
     followersCount: 15,
     followingCount: 3,
     messages: [
-      {
+      { name: 'Anakin Skywalker',
+        username: 'Anakin',
         message: 'Segundo Mensaje',
         repliesCount: 5
       },
       {
+        name: 'Anakin Skywalker',
+        username: 'Anakin',
         message: 'Primer Mensaje',
         repliesCount: 3
       }
@@ -29,35 +33,41 @@ const UserPage = ({params}: {params: {username: string}}) => {
 
   return (
     <main className="flex flex-col bg-gray-100 p-8">
-      <section className='flex flex-col mb-8'>
-      <div className="rounded-full p-6 bg-gray-300 w-20 text-center mb-5">
-        <span className="font-semibold text-lg">JD</span>
-      </div>
-      <h2 className="font-semibold text-lg mb-1">{user.name}</h2>
-      <div className="text-md mb-4 text-gray-600 cursor-pointer">
-        @<Link href={`/users/${user.username}`}>{user.username}</Link>
-      </div>
-      <div className="mb-4">{user.bio}</div>
-      <div className="flex justify-between mb-4">
-        <div>
-          <span className="font-semibold">{user.followersCount} Followers</span>
+      <section className="flex flex-col mb-8">
+        <div className="rounded-full p-6 bg-gray-300 w-20 text-center mb-5">
+          <span className="font-semibold text-lg">AS</span>
         </div>
-        <div>
-          <span className="font-semibold">{user.followingCount} Following</span>
+        <h2 className='mb-1'>{user.name}</h2>
+        <div className="text-md mb-4 text-gray-600 cursor-pointer">
+          @<Link href={`/users/${user.username}`}>{user.username}</Link>
         </div>
-      </div>
+        <div className="mb-4">{user.bio}</div>
+        <div className="flex justify-between mb-4">
+          <div>
+            <span className="font-semibold">
+              {user.followersCount} Followers
+            </span>
+          </div>
+          <div>
+            <span className="font-semibold">
+              {user.followingCount} Following
+            </span>
+          </div>
+        </div>
       </section>
 
-      <div className='flex justify-evenly mb-4'>
-        <div className='cursor-pointer border-b-2 border-blue-400'>Messages</div>
-        <div className='cursor-pointer'>Replies</div>
+      <div className="flex justify-evenly mb-4">
+        <div className="cursor-pointer border-b-2 border-blue-400">
+          Messages
+        </div>
+        <div className="cursor-pointer">Replies</div>
       </div>
 
       <div>
         {user.messages.map((message, index) => (
-          <div key={index}>
-            <div>{message.message}</div>
-          </div>
+          <Message key={`${index}`}
+            message={message}
+          />
         ))}
       </div>
     </main>
